df6799d055948376e6bd0e1caa6e4e649cfb5b28
Julian George <jules104@gmail.com>
2020-04-22 11:54:08 +0100

    profile route using user instead of code yellow

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,17 +5,15 @@ const authRouter = express.Router();
 
 function router() {
   authRouter.route('/signup').post((req, res) => {
-    console.log('whats going on?');
     console.log(req.body);
     req.login(req.body, () => {
       res.redirect('/auth/profile');
-
-      // eslint-disable-next-line no-shadow
-      authRouter.route('/profile').get((req, res) => {
-        // res.json(req.user);
-        res.send('code yellow');
-      });
     });
+  });
+  // eslint-disable-next-line no-shadow
+  authRouter.route('/profile').get((req, res) => {
+    console.log(req.user);
+    res.json(req.user);
   });
   return authRouter;
 }
